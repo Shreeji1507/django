230dbdc1d9ec873583cb0e3a1f0cab8114282ca4
Shreeji Patel <shreejibharatkumar.patel@dcmail.ca>
2019-07-06 16:37:07 -0400

    Added some views and view inheritance

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-
+posts = [
+    {
+        'author': 'Shreeji Patel',
+        'title' : 'First Post',
+        'content' : 'First Post Content',
+        'date_posted': 'August 27, 2019'
+    },
+    {
+        'author': 'Someone Patel',
+        'title' : 'Second Post',
+        'content' : 'Second Post Content',
+        'date_posted': 'August 27, 2019'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context= {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1> Blog About </h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
+
